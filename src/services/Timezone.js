6f442eda0b7ac6cbf5e0fa272d8f46d2362f4c51
Daniel James <daniel.james@scorebig.com>
2016-02-06 20:36:27 -0800

    Fix for Timezone to properly reject error messages

--- src/services/Timezone.js
+++ src/services/Timezone.js
@@ -1,10 +1,23 @@
 import timezoner from "google-timezone-api";
-import Promise from "bluebird";
 import makeRateLimited from "./RateLimitCurrier";
 import makeCached from "./FileBackedCacheCurrier";
 
-let rateLimited = makeRateLimited((latitude, longitude, timestamp) => timezoner({
-	location: `${latitude},${longitude}`,
-	timestamp: timestamp
-}), 10);
+
+let timezone = function(latitude, longitude, timestamp) {
+	return timezoner({
+		location: `${latitude},${longitude}`,
+		timestamp: timestamp
+	})
+		.then(result => {
+			return new Promise((resolve, reject) => {
+				if (result.errorMessage)
+					reject(result);
+				else
+					resolve(result);
+			})
+
+		});
+}
+let rateLimited = makeRateLimited(timezone, 10);
+
 export default makeCached(rateLimited, "timezone");
