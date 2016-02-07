1ad409615f4189cd3d5be1a9ba74da17934c9ef4
Daniel James <daniel.james@scorebig.com>
2016-02-06 21:19:31 -0800

    Added script to create posts from JSON file

---
+++ src/index-create-posts.js
@@ -0,0 +1,74 @@
+import fs from "fs";
+import path from "path";
+import Promise from "bluebird";
+import program from "commander";
+
+let readFile = Promise.promisify(fs.readFile);
+let writeFile = Promise.promisify(fs.writeFile);
+
+program
+	.version("1.0.0")
+	.option("<jsonFileName> <postsPath>")
+	.action((jsonFileName, postsPath) => {
+		readFile(jsonFileName)
+			.then(JSON.parse)
+			.map(record => {
+				return {
+					fileName: path.join(postsPath, generateUniqueFileName(record)),
+					content: generateContent(record),
+				};
+			})
+			.map(file => {
+				return writeFile(file.fileName, file.content)
+					.then(() => file);
+			})
+			.then(files => {
+				console.log(`Created ${files.length} posts...`);
+			});
+	})
+	.parse(process.argv);
+
+var fileNameCache = {};
+
+function generateFileName(record, words) {
+	var dateString = record.date.date.slice(0,10);
+	var location = `${record.location.state}-${record.location.city}`;
+	var title = record.report.narrative
+		.split(/\W+/)
+		.slice(0,words)
+		.join('-');
+
+	return `${dateString}-${location}-${title}`
+		.toLowerCase()
+		.replace(/[^a-z\d]+/gi, "-") + ".md";
+}
+
+function generateUniqueFileName(record) {
+	var words = 6;
+	var fileName = generateFileName(record, words);
+	while (fileName in fileNameCache) {
+		words++;
+		fileName = generateFileName(record, words);
+	}
+
+	fileNameCache[fileName] = true;
+
+	return fileName;
+}
+
+function generateContent(record) {
+	var city = record.location.city || "Not specified";
+	return `---
+layout: post
+title: ${city}, ${record.location.state} - ${record.report.narrative.split(/\W+/).slice(0,15).join(" ")}
+categories: ${record.location.state},${city}
+latitude: ${record.location.latitude}
+longitude: ${record.location.longitude}
+city: ${city}
+state: ${record.location.state}
+date: ${record.date.date}
+timeZoneName: ${record.date.timeZoneName}
+---
+
+${record.report.narrative}`;
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import program from "commander";
 
 program
 	.version("1.0.0")
-	.command("parse <file>", "Parse UAV events spreadsheets")
 	.command("parse <file>", "Parse UAV events spreadsheets into JSON file")
+	.command("create-posts <file>", "Create Jekyll posts from JSON file")
 	.parse(process.argv);
